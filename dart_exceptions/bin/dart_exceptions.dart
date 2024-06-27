543a7047bfd1a3eb9f53a098312eecb2f6d22910
Gustavo.Fonseca <gustavo.fonseca@sicoob.com.br>
2024-06-26 23:35:09 -0300

    adding assert check in constructor Account

--- dart_exceptions/bin/dart_exceptions.dart
+++ dart_exceptions/bin/dart_exceptions.dart
@@ -9,15 +9,9 @@ void main() {
   BankController bankController = BankController();
 
   // Adicionando contas
-  bankController.addAccount(
-      id: "Ricarth",
-      account:
-          Account(name: "Ricarth Lima", balance: 400, isAuthenticated: true));
+  bankController.addAccount(id: "Ricarth", account: Account(name: "Ricarth Lima", balance: 400, isAuthenticated: true));
 
-  bankController.addAccount(
-      id: "Kako",
-      account:
-          Account(name: "Caio Couto", balance: 600, isAuthenticated: true));
+  bankController.addAccount(id: "Kako", account: Account(name: "Caio Couto", balance: 600, isAuthenticated: true));
 
   try {
     // Fazendo transferência
--- dart_exceptions/bin/models/account.dart
+++ dart_exceptions/bin/models/account.dart
@@ -3,10 +3,10 @@ class Account {
   double balance;
   bool isAuthenticated;
 
-  Account(
-      {required this.name,
-      required this.balance,
-      required this.isAuthenticated});
+  Account({required this.name, required this.balance, required this.isAuthenticated}):
+      assert(name.isNotEmpty, 'O nome não pode ser vazio'),
+      assert(balance >= 0, 'O saldo não pode ser menor 0');
+
 
   editBalance({required value}) {
     balance = balance + value;
